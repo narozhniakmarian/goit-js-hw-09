317a39e5c1e2f7c455c0701866c89af9ba1ced94
narozhniakmarian <narozhniakmarian@gmail.com>
2025-06-28 19:21:18 +0200

    form

---
+++ src/js/2-form.js
@@ -0,0 +1,45 @@
+
+
+export const formData = {
+    email: "",
+    message: ""
+};
+export function localStorageInput() {
+    const form = document.querySelector('.feedback-form');
+    if (!form) return;
+
+    const emialInput = form.elements.email;
+    const texareaInput = form.elements.message;
+    const localStorageKey = "feedback-form-state";
+    const saveData = localStorage.getItem(localStorageKey);
+    if (saveData) {
+        const dataParse = JSON.parse(saveData);
+        formData.email = dataParse.email || "";
+        formData.message = dataParse.message || "";
+        emialInput.value = formData.email;
+        texareaInput.value = formData.message;
+    }
+
+    form.addEventListener('input', (event) => {
+        console.log('Input event:', event.target.name, event.target.value);
+
+        formData[event.target.name] = event.target.value;
+        localStorage.setItem(localStorageKey, JSON.stringify(formData));
+    });
+
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if (formData.email.trim() === "" || formData.message.trim() === "") {
+            alert("Fill please all fields");
+            return;
+        }
+
+        console.log("Form submitted with data:", formData);
+        localStorage.removeItem(localStorageKey);
+        form.reset();
+
+    });
+
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import hoverA from './js/index.js';
 import gallerySlyds from './js/1-gallery.js';
 
+import { formData, localStorageInput } from './js/2-form.js';
 
 
 hoverA();
 gallerySlyds();
+
+localStorageInput();
